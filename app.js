9eb1876418ec52743483e291544c0d9af7eb595a
thaibalong7 <thai.balong.7@gmail.com>
2018-12-19 21:34:54 +0700

    + add api: login, get payment by id

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 db.sequelize.sync();
-var server_port = process.env.YOUR_PORT || process.env.PORT || 9000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next();
+});
+
 require('./app/router/users')(app);
 require('./app/router/payments')(app);
 require('./app/router/posts')(app);
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 9000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
 var server = app.listen(server_port, server_host, function () {
     console.log(`App listening at port ${server_port}`)
 });
--- app/controller/users.js
+++ app/controller/users.js
@@ -1,5 +1,14 @@
 const db = require('../config/db.config.js');
 
 exports.login = (req, res) => {
-
+    if (typeof req.body.public_key === 'undefined') {
+        return res.status(400).json({ msg: 'Params Invalid' })
+    }
+    const account = await db.users.findByPk(req.body.public_key)
+    if (!account) {
+        return res.status(400).json({ msg: 'Account does not exists' })
+    }
+    else {
+        return res.status(200).json({ account_info: account })
+    }
 }
