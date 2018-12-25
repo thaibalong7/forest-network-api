ba84b2c648eedb0850b735e68dbe4b670010d09d
thaibalong7 <thai.balong.7@gmail.com>
2018-12-25 12:03:22 +0700

    ! stop websocket listens newblock

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const db = require('./app/config/db.config');
-const { readAllBlock } = require('./ReadAllBlock')
+// const { readAllBlock } = require('./ReadAllBlock')
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,5 +24,5 @@ var server_host = process.env.YOUR_HOST || '0.0.0.0';
 var server = app.listen(server_port, server_host, function () {
     console.log(`App listening at port ${server_port}`)
 });
-readAllBlock();
+// readAllBlock();
 module.exports = server;
